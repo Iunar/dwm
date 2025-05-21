0b2cfc5ffad5ce613f4d9a93eedbdad3720bb6be
Tyler J <tjamesjob@gmail.com>
2025-05-20 19:35:35 -0700

    ???

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 0;        /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -42,7 +42,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -50,10 +50,10 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
  static const Layout layouts[] = {
     /* symbol     arrange function */
+    { "[\\]",     dwindle },
     { "[]=",      tile },    /* first entry is default */
     { "[M]",      monocle },
     { "[@]",      spiral },
-    { "[\\]",     dwindle },
     { "H[]",      deck },
     { "TTT",      bstack },
     { "===",      bstackhoriz },
